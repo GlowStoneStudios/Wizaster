0d8ef6dcbf385725ea6ba8ae4f124939419bcb7c
Matías Montenegro <matias@framematt.com>
2016-03-08 22:24:56 -0300

    Avance 08-03
    comportamiento protagonista, y un ready-go

---
+++ INFINITE_RUNNER/Assets/_Code/Gameplay/GameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	/* Atributos */
+	[Header ("Game Basics")]
+	public int timeToStart;
+
+	PlayerBehaviour playerCharacterScript;
+	bool startClock, levelStarted;
+
+	/* Aplicacion al motor */
+	void Start () 
+	{
+		// Mem Cache
+		playerCharacterScript = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerBehaviour>();
+
+		// Prevension
+		startClock = true;
+	}
+
+	void Update () 
+	{
+		// Llamamos al timer
+		if (!levelStarted) InitialCounter (startClock, timeToStart);
+	}
+
+	/* Metodos de la clase */
+	void InitialCounter (bool started, int seconds)
+	{
+		if (started) 
+		{
+			// Timer
+			if (Time.timeSinceLevelLoad > seconds)
+			{
+				//Debug.Log ("Level Started!");
+				playerCharacterScript.PlayerGo ();
+				levelStarted = true;
+			}
+		}
+	}
+}
---
+++ INFINITE_RUNNER/Assets/_Code/Gameplay/PlayerBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour 
+{
+	/* Atributos */
+	[Header("Movement Behaviour")]
+	public float speed;
+	public float playerGravity;
+
+	public bool canMove { get; private set; }
+
+	float actualSpeed, actualPlayerGravity;
+	bool grounded;
+	Transform selfTrans;
+
+	/* Aplicacion al motor */
+	void Start ()
+	{
+		// Mem Cache
+		selfTrans = this.transform;
+
+		// Prevension
+		if (speed == 0.0f) speed = 2.0f;
+		if (playerGravity == 0.0f) playerGravity = 9.8f; 
+		actualPlayerGravity = playerGravity;
+		actualSpeed = speed;
+
+		grounded = true;
+		//Debug.Log ("Velocidad = " + actualSpeed + "\nGravedad = " + actualPlayerGravity);
+	}
+	
+	void Update ()
+	{
+		// Cuando el personaje se pueda mover
+		if (canMove)
+		{
+			/* Condiciones de movimiento */
+			// Gravedad
+			if (grounded)
+			{
+				actualPlayerGravity = 0.0f;
+			} 
+			else 
+			{
+				actualPlayerGravity = playerGravity;
+			}
+
+			// Se desplaza en Z hacia adelante, y leemos la gravedad (para saltos o caidas que vayan a haber)
+			selfTrans.Translate (0, actualPlayerGravity, actualSpeed * Time.deltaTime);
+		}
+	}
+
+	/* Metodos de la clase */
+	public void PlayerGo ()
+	{
+		canMove = true;
+	}
+
+	/* Triggers */
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Default")
+		{
+			grounded = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.tag == "Default")
+		{
+			grounded = false;
+		}
+	}
+}
