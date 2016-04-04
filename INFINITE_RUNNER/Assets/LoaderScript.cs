cd86084f453c0b9b693afd4134898f1969134176
Matías Montenegro <matias@framematt.com>
2016-04-04 16:08:41 -0400

    dasd
    as

---
+++ INFINITE_RUNNER/Assets/LoaderScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoaderScript : MonoBehaviour
+{
+	AsyncOperation Async = null;
+
+	void Start () {
+		StartCoroutine (LoadingLevel("gameplay"));
+	}
+
+	IEnumerator LoadingLevel (string i) {
+		Async = Application.LoadLevelAsync (i);
+		yield return Async;
+	}
+}
---
+++ INFINITE_RUNNER/Assets/_Code/TestRestart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestRestart : MonoBehaviour {
+
+	public Image button;
+	public GameObject text;
+	bool canRestart;
+
+	void Update () {
+		if (!PlayerBehaviour.instance.isAlive) {
+			button.enabled = true;
+			text.SetActive (true);
+			canRestart = true;
+		}
+	}
+
+	public void RestartLevel () {
+		if (canRestart) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("gameplay");
+		}
+	}
+}
