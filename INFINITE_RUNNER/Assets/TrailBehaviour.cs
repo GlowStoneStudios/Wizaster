7aae9bf634369f95c58ebe5028262d071dc87ba7
Matías Montenegro <matias@framematt.com>
2016-04-15 15:58:08 -0400

    Run trails update (test)
    - añadidas animaciones varias del efecto.

---
+++ INFINITE_RUNNER/Assets/TrailBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailBehaviour : MonoBehaviour
+{
+	/* Atributos */
+	[Tooltip ("Trail Type va del 1 al 5.")]
+	public float trailType;
+
+	Animator trailAnim;
+	Transform selfTrans;
+
+	/* Aplicacion al motor */
+	void Awake () {
+		// Mem cache
+		trailAnim = GetComponent<Animator> ();
+		selfTrans = this.transform;
+	}
+
+	void Start () {
+		// Prevension
+		if (trailType > 4 || trailType < 0) {
+			trailType = Random.Range (1, 5);
+			trailType = (int) trailType;
+		}
+
+		// Trail type setup.
+		trailAnim.SetFloat ("trails", trailType);
+
+		// Sprite align
+		if (trailType == 2.0f || trailType == 3.0f) {
+			selfTrans.position = new Vector3 (-0.082f, -0.2f, -0.776f);
+			selfTrans.rotation = Quaternion.Euler (new Vector3 (90, 260, 0));
+		} else {
+			selfTrans.position = new Vector3 (0f, -0.2f, -0.7f);
+			selfTrans.rotation = Quaternion.Euler (new Vector3 (90, 290, 0));
+		}
+
+		// Trail type setup.
+		trailAnim.SetFloat ("trails", trailType);
+	}
+}
