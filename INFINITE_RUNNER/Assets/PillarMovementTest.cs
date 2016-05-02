66b44046ea4f6c348dc566f4ef6bbab71a8aaa97
Matías Montenegro <matias@framematt.com>
2016-05-02 13:56:38 -0400

    PilaresAnimados
    - Pilares leen el tilt up del jugador y ejecutan una animacion para
    cambiar sus posiciones.

---
+++ INFINITE_RUNNER/Assets/PillarMovementTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillarMovementTest : MonoBehaviour
+{
+	/* Atributos */
+	public string triggerName;
+	Transform selfTrans;
+	Vector3 starterPos;
+	Animator selfAnim;
+
+	/* Aplicacion al motor */
+	void Start () {
+		selfTrans = this.transform;
+		selfAnim = GetComponent<Animator> ();
+	}
+
+	void OnMouseDown () {
+		starterPos = selfTrans.position;
+	}
+
+	void OnMouseUp () {
+		if (selfTrans.position.y >= starterPos.y) {
+			selfAnim.SetTrigger (triggerName);
+		}
+	}
+}
